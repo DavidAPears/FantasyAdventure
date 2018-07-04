d72526a798ced5c0b7b88a06277f1e85da3fec7d
Stephen Rooney <s.f.rooney@gmail.com>
2018-07-04 13:44:28 +0100

    barbrafuwbf

--- src/test/java/BarbarianTest.java
+++ src/test/java/BarbarianTest.java
@@ -1,2 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BarbarianTest {
+
+
+
+    Club club;
+    Barbarian barbarian;
+
+    @Before
+    public void before() {
+        club = new Club();
+        barbarian = new Barbarian("Barry", 80, club);
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Barry", barbarian.getName());
+    }
+
+    @Test
+    public void getHealthPoints() {
+        assertEquals(80, barbarian.getHealthPoints());
+    }
+
+    @Test
+    public void takeDamage() {
+        barbarian.takeDamage(10);
+        assertEquals(70, barbarian.getHealthPoints());
+    }
+
+    @Test
+    public void giveHealth() {
+        barbarian.giveHealth(10);
+        assertEquals(90, barbarian.getHealthPoints());
+    }
 }
